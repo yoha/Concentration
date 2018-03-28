148eb7ea93951326a739ae1ccb7f7b8e4338d74c
Yohannes Wijaya <readyforservice@icloud.com>
2018-03-28 17:36:22 +0700

    Introduction to iOS 11, Xcode 9, and Swift 4

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -9,17 +9,46 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    // MARK: - Properties
+    
+    var flipCount = 0 {
+        didSet {
+            flipCountLabel.text = "FLIPS: \(flipCount)"
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    let emojiSelections = ["👍🏼", "👏🏼", "👍🏼", "👏🏼"]
+    
+    // MARK: - IBOutlet Properties
+    
+    @IBOutlet weak var flipCountLabel: UILabel!
+    @IBOutlet var cardButtons: [UIButton]!
+    
+    // MARK: - IBAction Methods
+    
+    @IBAction func touchCardDidTouch(_ sender: UIButton) {
+        flipCount += 1
+        if let cardNumber = cardButtons.index(of: sender) {
+            flipCard(withEmoji: emojiSelections[cardNumber], on: sender)
+        }
+        else {
+            print("Card is not found...")
+        }
+        
     }
-
-
+    
+    // MARK: - Helper Methods
+    
+    func flipCard(withEmoji emoji: String, on button: UIButton) {
+        if button.currentTitle == emoji {
+            button.setTitle("", for: .normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+        }
+        else {
+            button.setTitle(emoji, for: .normal)
+            button.backgroundColor = UIColor.white
+        }
+    }
 }
 
